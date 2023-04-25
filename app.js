8b575b783cde501d68faf0a00d064596242b9158
HR <hr@batch.lan>
2023-04-25 08:55:15 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+let x = 10
+let y = 20
+
+var total1 = x + y 
+var minus = x - y 
+console.log('total add', total1);
+console.log('Minus valuse  =',minus);
+
+
+// funtion code 
+
+let add = (num1, num2, num3) =>  {
+    return (num1 + num2 ) * 2
+}
+
+let funRes = add(5 , 6)
+
+console.log(funRes);
+
+// let h1Cli = document.getElementByClassName('h1C')
+// console.log(h1Cli);
+
+// Next function 
+function testFun (a) {
+    return a * a 
+}
+
+
+console.log('Test funstion A - ', testFun(3));
+
+var testData = {
+    name : 'simeon',
+    age : 30
+}
+
+console.log('Name check', testData.name);
+
+
+
+
